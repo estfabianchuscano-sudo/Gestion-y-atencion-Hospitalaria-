573ba473982c06c27b031d589e9e4af32296fc20
estfabianchuscano-sudo <est.fabian.chuscano@unimilitar.edu.co>
2026-03-18 19:35:56 -0500

    segundo commit

---
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,48 @@
+package com.mycompany.proyecto;
+
+import com.murcia.utils.*;
+
+public class Proyecto {
+
+    public static void main(String[] args) {
+        
+    String option[] = {"1. Registrar pacientes ", "2. Asignsr citas", "3. Atender pacientes", "4. Ver informacion del paciente", "5. 5alir del sistema"};
+    Menu menu = new Menu(option, 'V', "   ", "MENU DE INICIO");   
+    
+     char sel;
+
+        do {
+            sel = menu.select("Seleccione su respuesta");
+
+            switch (sel) {
+
+                case '1':
+                    System.out.println("Registrar paciente");
+                    break;
+
+                case '2':
+                    System.out.println("Asignar cita");
+                    break;
+
+                case '3':
+                    System.out.println("Atender paciente");
+                    break;
+
+                case '4':
+                    System.out.println("Ver información");
+                    break;
+
+                case '5':
+                    System.out.println("Saliendo del sistema");
+                    break;
+
+                default:
+                    System.out.println("Opción inválida");
+
+            }
+
+        } while (sel != '5');
+    
+    
+    }
+}
